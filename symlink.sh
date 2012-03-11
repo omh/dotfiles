7b8852e72ca535a622b64fdf8f977497bd07f8a0
Ole Morten Halvorsen <olemortenh@gmail.com>
2012-03-11 21:35:03 +1100

    Janus refactor

--- symlink.sh
+++ symlink.sh
@@ -3,17 +3,9 @@
 git submodule init
 git submodule update
 
-rm -fr ~/.vim
-rm -f ~/.vim
-ln -sf `pwd`/vim ~/.vim
-ln -sf `pwd`/vimrc ~/.vimrc
-ln -sf `pwd`/gvimrc ~/.gvimrc
+ln -sf `pwd`/vim ~/.janus
+ln -sf `pwd`/vimrc.before ~/.vimrc.before
+ln -sf `pwd`/vimrc.after ~/.vimrc.after
+ln -sf `pwd`/gvimrc.after ~/.gvimrc.after
 ln -sf `pwd`/zshrc ~/.zshrc
 ln -sf `pwd`/oh-my-zsh ~/.oh-my-zsh
-
-# Generate help tags
-vim -c "call pathogen#helptags() | :q"
-
-cd ~/.vim/bundle/commandt/
-rake make
-
