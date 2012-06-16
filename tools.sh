c55f6f33f01eaca215afa83d7453f16766fd3fba
Ole Morten Halvorsen <olemortenh@gmail.com>
2012-06-16 14:59:02 +1000

    List of commonly used tools.

---
+++ tools.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# zsh
+brew install zsh
+
+# macvim
+brew install macvim
+
+# proper vim with ruby support
+brew install https://raw.github.com/Homebrew/homebrew-dupes/master/vim.rb
+
+# rbenv + ruby-build
+brew install rbenv
+brew install ruby-build
+
+# tig - git browser
+brew install tig
+
+# ctags
+brew install ctags
+
+# ack 
+brew install ack
+
+# https://github.com/tpope/gem-ctags
+gem install gem-ctags
+
+# https://github.com/tpope/gem-ctags
+gem install gem-ctags
+
