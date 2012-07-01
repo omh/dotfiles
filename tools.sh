f1facc603381e3e373811e9fc6456a28ab5a0a88
Ole Morten Halvorsen <omh@sitepoint.com>
2012-07-01 17:31:04 +1000

    Added line to symlink vi -> vim.

--- tools.sh
+++ tools.sh
@@ -9,6 +9,9 @@ brew install macvim
 # proper vim with ruby support
 brew install https://raw.github.com/Homebrew/homebrew-dupes/master/vim.rb
 
+# Set up local symlink for 'vi'.
+sudo ln -s /usr/local/bin/vim /usr/local/bin/vi
+
 # rbenv + ruby-build
 brew install rbenv
 brew install ruby-build
